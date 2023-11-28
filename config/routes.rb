2a9601cf088eb7a49b3c4f1b29d1d5331fd0c1e1
anshuman7negi <anshumannegi777@gmail.com>
2023-11-28 19:35:27 +0530

    created api to get random message

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  namespace :api do
+    get 'random_greeting', to: 'greetings#random_greeting'
+  end
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
